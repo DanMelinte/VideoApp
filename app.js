820a32e8303a1d7db7591fe2288fac67e71ad223
DanMelinte <DanMelinte@yandex.ru>
2018-10-30 18:34:13 +0200

    Temp

--- app.js
+++ app.js
@@ -1,19 +1,33 @@
 const express = require("express");
-
 const app = express();
 
+var exphbs  = require('express-handlebars');
+var hbs = exphbs.create({ /* config */ });
+
+
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
+
+app.set('view engine', 'handlebars');
+
 app.get('/', (req, res) => {
-    res.send('INDEX')
+    const title = "Welcome";
+    res.render('index', {
+        title : title
+    });
 });
 
 app.get('/about', (req, res) => {
-    res.send('ABOUT');
+    res.render('ABOUT');
 });
 
-const port = 5000;
+app.use(function(req, res, next) {
+    req.name = "Nikita";
+ })
+
+const port = 5002;
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`); 
 });
-
-
